a26f5a39a43049b80e5de50a58a8f7f71c38342d
Ryan <ryan.elr@outlook.com>
2018-06-22 08:46:42 +0200

    ex2

---
+++ script.js
@@ -0,0 +1,5 @@
+//Déclaration des variables et assignation de leurs valeurs.//
+var lastname = 'El-rhichachmi\n';
+var firstname = 'Ryan\n';
+var city = 'Noyon';
+alert('Nom : ' + lastname + '\n' + 'Prénom : ' + firstname + '\n' + 'Ville : ' + city);
